be0e9b4213e28feac043c81aa4de8564d63a7f88
concreted <arichuang@gmail.com>
2014-12-23 12:25:43 -0800

    Update gulpfile and package.json for browserify

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,21 @@ var jshint = require('gulp-jshint');
 var browserify = require('browserify');
 var transform = require('vinyl-transform');
 var uglify = require('gulp-uglify');
+var commonjsify = require('commonjsify');
 
 var paths = {
-  client: './client/app/app.js',
-  bundle: './client/bundle.js'
+  client: './client/src/app.js',
+  bundle: './client/dist'
 };
 
 gulp.task('browserify', function() {
   var browserified = transform(function(filename) {
     var b = browserify(filename);
+    b.require(require.resolve('angular'), {entry: true, expose: 'angular'});
+    b.transform(commonjsify({
+      'angular': 'angular',
+      //'angular-ui-router': 'angular.module(\'ui.router\')'
+    }));
     return b.bundle();
   });
 
@@ -21,3 +27,5 @@ gulp.task('browserify', function() {
   //.pipe(uglify())
   .pipe(gulp.dest(paths.bundle));
 });
+
+gulp.task('default', ['browserify']);
